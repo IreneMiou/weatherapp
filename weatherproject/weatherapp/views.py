1e3dac046eefcfad8a2762e51fe6206006ef2e53
irene.psomiadou <eirini.psomiadou@deliveryhero.com>
2022-03-07 17:19:34 +0100

    changed quote-font , added error handling when api is offline

--- weatherproject/weatherapp/views.py
+++ weatherproject/weatherapp/views.py
@@ -1,22 +1,19 @@
+from re import A
 from django.shortcuts import render
 import requests
 from datetime import datetime, date
 import random
 
-quotes = [
-    '<quote><h3>"There’s something about arriving in new cities, wandering empty streets with no destination. I will never lose the love for the arriving, but I\'m born to leave."</h3></quote> -Charlotte Eriksson, Empty Roads & Broken Bottles',
-    '<quote><h3>"What strange phenomena we find in a great city, all we need do is stroll about with our eyes open. Life swarms with innocent monsters."</h3></quote> ― Charles Baudelaire',
-    '<quote><h3>"Cities, like dreams, are made of desires and fears, even if the thread of their discourse is secret, their rules are absurd, their perspectives deceitful, and everything conceals something else."</h3></quote> ― Italo Calvino, Invisible Cities',
-    '<quote><h3>"I love New York, even though it isn\'t mine, the way something has to be, a tree or a street or a house, something, anyway, that belongs to me because I belong to it."</h3></quote> ― Truman Capote',
-    "<quote><h3>\"I don't know what London's coming to — the higher the buildings the lower the morals.\" ― Noël Coward, Collected Sketches and Lyrics",
-    '<quote><h3>A city isn’t so unlike a person. They both have the marks to show they have many stories to tell. They see many faces. They tear things down and make new again."</h3></quote> ― Rasmenia Massoud, Broken Abroad',
-]
-
 
 def getrandomquote():
     API_KEY = "9P88EJr9.6adqVeoPQI25erodSuZ2fMre3MOhpC27"
-    URL = "http://127.0.0.1:8000/city_quotes/"
-    quotes = requests.get(url=URL, headers={"Authorization": "Api-Key " + API_KEY})
+    URL = "http://127.0.0.1:8001/city_quotes/"
+
+    try:
+        quotes = requests.get(url=URL, headers={"Authorization": "Api-Key " + API_KEY})
+    except:
+        return "Our API Server is down so imagine there is a fancy quote here.. "
+
     quote_data = quotes.json()["data"]
     quote_texts = []
     for quote in quote_data:
@@ -54,7 +51,3 @@ def index(request):
             "quote": getrandomquote(),
         },
     )
-
-
-# datetime.utcfromtimestamp(sunrise).strftime(%H:%M:%S)
-# datetime.utcfromtimestamp(sunset).strftime(%H:%M:%S)
